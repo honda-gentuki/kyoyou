fde75e4bffa73eb4113d366261928c9ee5b24090
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-25 13:42:56 +0900

    usersテーブルの作成

--- db/migrate/20220525043739_devise_create_users.rb
+++ db/migrate/20220525043739_devise_create_users.rb
@@ -6,6 +6,18 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :first_name_kana,    null: false
+      t.string :last_name_kana,     null: false
+      t.string :age
+      t.string :sex
+      t.string :school
+      t.string :subject
+      t.string :club
+      t.text   :introduce
+      t.string :image
 
       ## Recoverable
       t.string   :reset_password_token
