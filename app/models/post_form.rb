cf8126912c5d3e7dd58aa0fef9f1f56c19ecfad6
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-26 13:58:53 +0900

    投稿をデータベースに保存する

--- app/models/post_form.rb
+++ app/models/post_form.rb
@@ -4,7 +4,6 @@ class PostForm
   attr_accessor(
     :id, :school_list, :course, :unit, :introduction, :introduction_time, :development, :development_time, :summary, :summary_time,
     :user_id, :created_at, :updated_at, :images,
-    :id, :name, :record_type, :record_id, :blob_id, :created_at
   )
 
   with_options presence: true do
@@ -17,10 +16,16 @@ class PostForm
   end
 
   validates :images, length: { maximum: 6, message: 'は6枚以下にしてください' }
-  
+
   with_options numericality: { only_integer: true, less_than_or_equal_to: 50 } do
     validates :introduction_time
     validates :development_time
     validates :summary_time
   end
+
+  def save
+    post = Post.create(school_list: school_list, course: course, unit: unit, introduction: introduction, introduction_time: introduction_time,
+                       development: development, development_time: development_time, summary: summary, summary_time: summary_time, images: images, user_id: user_id)
+  end
+
 end
