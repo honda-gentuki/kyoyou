35b8d5b25ae2c4c8c831b0aa91468b10e9c79c1c
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-26 17:22:19 +0900

    モデルにImageUploderを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
 
+  mount_uploader :image, ImageUploader
+
   with_options presence: true do
     validates :nickname
     validates :password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'には英字と数字の両方を含めて設定してください' },
