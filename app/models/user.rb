f9e88077475d8de68ad796461cf6ae189e121b19
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-27 15:28:53 +0900

    フォロー管理のアソシエーション作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :posts, through: :likes
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :following_relationships
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships
 
   mount_uploader :image, ImageUploader
 
