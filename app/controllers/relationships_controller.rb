2b4f3e2eeb0837e8538da15c076177f57c4ac341
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-27 15:35:21 +0900

    フォロー機能コントローラー作成

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,12 @@
+class RelationshipsController < ApplicationController
+  def create
+    @user = User.find(params[:following_id])
+    current_user.follow(@user)
+    @user.create_notification_follow!(current_user)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    current_user.unfollow(@user)
+  end
+end
