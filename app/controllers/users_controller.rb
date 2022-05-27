464ec0f275950ebb9f5dfabc350a7110119adb28
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-27 10:51:52 +0900

    いいね一覧ページのコントローラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :likes]
+
   def show
+  end
+
+  def likes
+  end
+  
+  private
+
+  def set_user
     @user = User.find(params[:id])
   end
 end
