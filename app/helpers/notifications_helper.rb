b3f7806d930f632848d2d831b4572721dd29605f
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-28 15:11:54 +0900

    通知のアクションによる条件分岐

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,33 @@
 module NotificationsHelper
+  def notification_form(notification)
+    @visiter = notification.visiter
+    @comment = nil
+    @chat = nil
+    your_post = link_to 'あなたの投稿', post_path(notification), style: 'font-weight: bold;'
+    @visiter_comment = notification.comment_id
+    @visiter_chat = notification.chat_id
+    case notification.action
+      when 'follow'
+        tag.a(notification.visiter.nickname, href: user_path(@visiter), style: 'font-weight: bold;') + 'があなたをフォローしました'
+        
+      when 'like'
+        tag.a(notification.visiter.nickname, href: user_path(@visiter),
+                                            style: 'font-weight: bold;') + 'が' + tag.a('あなたの投稿', href: post_path(notification.post_id),
+                                                                                                  style: 'font-weight: bold;') + 'にいいねしました'
+      when 'comment'
+        @comment = Comment.find_by(id: @visiter_comment)&.content
+        tag.a(@visiter.nickname, href: user_path(@visiter),
+                                style: 'font-weight: bold;') + 'が' + tag.a('あなたの投稿', href: post_path(notification.post_id),
+                                                                                      style: 'font-weight: bold;') + 'にコメントしました'
+      when 'chat'
+        @chat = Chat.find_by(id: @visiter_chat)&.message
+        tag.a(@visiter.nickname, href: chat_path(@visiter),
+                               style: 'font-weight: bold;') + 'が' + tag.a('あなたの投稿', href: chat_path(notification.chat_id),
+                                                                                    style: 'font-weight: bold;') + 'にコメントしました'
+      end
+  end
+
+  def unchecked_notifications
+    @notifications = current_user.passive_notifications.where(checked: false)
+  end
 end
