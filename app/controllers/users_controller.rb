379ccb45bc847b64b5df5a6b5fed7392c605e052
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-28 10:08:38 +0900

    並び順といいね一覧、ユーザー詳細一覧のページネーション削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,6 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :likes, :following, :followers]
 
   def show
-    @post = Post.where(user_id: current_user.id).includes(:user).order('created_at DESC').page(params[:page]).reverse_order
     @follower_users = @user.followers
     @following_users = @user.following
   end
@@ -25,7 +24,6 @@ class UsersController < ApplicationController
   end
 
   def likes
-    @posts = Post.all.order('created_at DESC').page(params[:page]).reverse_order
   end
 
   private
