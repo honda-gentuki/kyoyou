3c2ee5f9b3d85e6df83f0d4ac94a08c12db3bc51
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-28 17:08:34 +0900

    パンくずリストの作成準備

---
+++ config/breadcrumbs.rb
@@ -0,0 +1,78 @@
+crumb :root do
+  link "トップページ", root_path
+end
+
+crumb :posts_pagination do |page|
+  link "#{page}ページ目"
+  parent :root
+end
+
+crumb :user_new do
+  link "新規登録", new_user_registration_path
+  parent :root
+end
+
+crumb :user_login do
+  link "ログイン", new_user_session_path
+  parent :root
+end
+
+crumb :new_post do
+  link "新規投稿", new_post_path
+  parent :root
+end
+
+crumb :notifications do
+  link "通知", notifications_path
+  parent :root
+end
+
+crumb :post_show do |post|
+  link "投稿詳細", post_path(post)
+  parent :root
+end
+
+crumb :search do |post|
+  link "検索結果", search_posts_path
+  parent :root
+end
+
+crumb :post_edit do |post|
+  link "投稿編集"
+  parent :post_show, post
+end
+
+crumb :user_show do |user|
+  link "#{user.nickname}先生のマイページ", user_path(user)
+  parent :root
+end
+
+crumb :user_edit do |user|
+  link "#{user.nickname}先生の編集"
+  parent :user_show, user
+end
+
+crumb :follows_user do |user|
+  link "フォロー一覧"
+  parent :user_show, user
+end
+
+crumb :followers_user do |user|
+  link "フォロワー一覧"
+  parent :user_show, user
+end
+
+crumb :like_index do |user|
+  link "いいね一覧"
+  parent :user_show, user
+end
+
+crumb :chat_index do |user|
+  link "チャット一覧"
+  parent :user_show, user
+end
+
+crumb :chat do |chat|
+  link "チャットページ"
+  parent :chat_index, chat
+end
