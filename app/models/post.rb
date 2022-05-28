e4fe50dc46c2539087a4f1660b015a39ef25a4e9
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-28 10:57:45 +0900

    いいねの通知メソッド作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,20 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes
   has_many_attached :images
+  has_many :notifications, dependent: :destroy
+
+  def create_notification_like!(current_user)
+    temp = Notification.where(["visitor_id = ? and visited_id = ? and post_id = ? and action = ? ", current_user.id, user_id, id, 'like'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        post_id: id,
+        visited_id: user_id,
+        action: 'like'
+      )
+      if notification.visitor_id == notification.visited_id
+        notification.checked = true
+      end
+      notification.save if notification.valid?
+    end
+  end
 end
