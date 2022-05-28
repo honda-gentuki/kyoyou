75705aa9812c9b075a4b3504a337d8acd1f2831c
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-28 11:24:36 +0900

    通知ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   end
   resources :relationships, only: [:create, :destroy]
   resources :chats, only: [:index, :show, :create]
+  resources :notifications, only: :index
 end
