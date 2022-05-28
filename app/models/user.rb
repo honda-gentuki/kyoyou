37efe05a6d914d83e91bef3b08ebe9651f3b906b
takuto_honda <krt.tc.se.1997@gmail.com>
2022-05-28 10:57:10 +0900

    通知関連のアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   has_many :user_rooms, dependent: :destroy
   has_many :rooms, through: :user_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
+  has_many :active_notifications, class_name: 'Notification', foreign_key: 'visiter_id', dependent: :destroy
+  has_many :passive_notifications, class_name: 'Notification', foreign_key: 'visited_id', dependent: :destroy
 
   mount_uploader :image, ImageUploader
 
