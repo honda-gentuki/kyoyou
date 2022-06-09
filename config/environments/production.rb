274621e0a40994cd6141f7fafd6c937e4ffbf9a2
takuto_honda <krt.tc.se.1997@gmail.com>
2022-06-09 20:48:00 +0900

    ipアドレス修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.action_cable.allowed_request_origins = ['https://【Herokukyoyou】.herokuapp.com', 'http://【Herokukyoyou】.herokuapp.com']
 
     ActionCable.server.config.disable_request_forgery_protection = true
-  config.action_cable.url = "ws://35.76.211.21/cable" 
+  config.action_cable.url = "ws:///13.113.200.90/cable" 
   config.action_cable.allowed_request_origins = ['http://13.113.200.90']
   
   # Settings specified here will take precedence over those in config/application.rb.
