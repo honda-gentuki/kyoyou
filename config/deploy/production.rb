f92e06a222d2e7f09f8531aef51b3ab1fc70d586
takuto_honda <krt.tc.se.1997@gmail.com>
2022-06-09 20:02:34 +0900

    ipアドレス変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '35.76.211.21', user: 'ec2-user', roles: %w{app db web}
+server '13.113.200.90', user: 'ec2-user', roles: %w{app db web}
