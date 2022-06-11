5f8572ac8f207a567a3bed6a8047debdbc1c3a34
takuto_honda <krt.tc.se.1997@gmail.com>
2022-06-11 15:57:35 +0900

    unicorn設定がいるの修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '13.113.200.90', user: 'ec2-user', roles: %w{app db web}
+server '175.41.219.237', user: 'ec2-user', roles: %w{app db web}
