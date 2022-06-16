fd1d2d99dcc95e219dc66a3c7c10e3e0389a1b1f
takuto_honda <krt.tc.se.1997@gmail.com>
2022-06-16 23:56:31 +0900

    未ログイン時は通知一覧ページに遷移できないよう記述

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,5 @@
 class NotificationsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = User.all
     @notifications = current_user.passive_notifications.page(params[:page]).per(20)
